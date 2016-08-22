bd92e8b559d3bebe4a3e26dede1c7e7c2e1664b6
lucl310 <lucthehuman21@gmail.com>
2016-08-22 12:30:12 -1000

    added other advanced js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+var open = function (re) {
+getElementsById('IT IS').innerHTML = "I hope you have a great 2016 aunty judy!! thank you for doing so much for us!!!"
+
+
+
+}
+
+if (open === true) {console.log()}
