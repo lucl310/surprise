53dc412cb4e0244d3e948db122139a2518e50e2e
lucl310 <lucthehuman21@gmail.com>
2016-09-10 08:55:31 -1000

    added ground + code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,38 @@
+// var open = 1+1;
+var x = 5
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+// ctx.fillStyle = "#FF0000";
+// ctx.fillRect(0,0,150,75);
 
-var open = function (re) {
-document.getElementsById('IT IS').innerHTML = "I hope you have a great 2016 aunty judy!! thank you for doing so much for us!!!"
+
+// ctx.draw = function() {
+    
+//     document.body.style.backgroundImage = "url('../assets/background.jpg')";
+    
+    // draw the car body
+    var canvas = document.getElementById("myCanvas").getContext("2d");
+     // fill(255, 0, 115);
+     canvas.strokeStyle="green";
+canvas.fillStyle="red";
+canvas.strokeRect(x,200,100,20);
+canvas.fillRect(x + 15,200,178,40);
+canvas.fillRect(95,95,30,30);
+//  rect(x, 200, 100, 20);
+     //   rect(x + 15, 178, 70, 40);
+     //    fill(77, 66, 66);
+     //     ellipse(x + 25, 221, 24, 24);
+     //      ellipse(x + 75, 221, 24, 24);
+     //       x = x + 1;
+    // document
+    // draw the wheels"
+    
+// };
+// var open = function (re) {
+// document.getElementsById('IT IS').innerHTML = "I hope you have a great 2016 aunty judy!! thank you for doing so much for us!!!"
 
 
 
-}
+// }
 
-if (open === true) {console.log(thank you)}
+// if (open === 2) {console.log("thank you")}
